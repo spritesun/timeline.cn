b04bbcbece2b387b400b0c75a2392267b33e5178
Long Sun <spritesun@gmail.com>
2010-12-20 11:10:23 +0800

    Initial timeline project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeline::Application.config.secret_token = '097b620d30a342f7febd2a04e643a404fc48e43c9175ef21335ca031a0ef55d1be583f41bb8a8511ab747bfcb0e922641a75ce9d95c0664872795ea9b5b569eb'
