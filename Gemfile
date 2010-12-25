602b6f79649a9fd5b4938a900eb370fa20956714
Long Sun <spritesun@gmail.com>
2010-12-25 17:57:02 +0800

    fetch already read book resources from douban then save them indo db

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
 gem 'sqlite3-ruby', '>=1.2.5', :require => 'sqlite3'
+gem 'json'
 
 group :development do
   gem 'rspec-rails', '>=2.3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Timeline::Application.routes.draw do
-  resources :books
+  resources :books, :only => "index" do
+    collection do
+      get 'sync'
+    end
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
